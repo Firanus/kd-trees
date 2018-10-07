753a7c51dceab979a4d355f60f3ec8f4bf40fe7e
Ivan Tchernev <itchernev@and.digital>
2018-10-07 17:56:18 +0100

    Implemented size, isEmpty, contains and insert methods of KdTree

--- src/main/java/com/ivantchernev/algorithms/KdTree.java
+++ src/main/java/com/ivantchernev/algorithms/KdTree.java
@@ -6,31 +6,72 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
-    // construct an empty set of points
-    public KdTree() {
 
+    private static class Node {
+        private static final boolean X_ORIENTED = true;
+        private static final boolean Y_ORIENTED = false;
+
+        private final Point2D p;      // the point
+        private final boolean isXOriented; // is this node x or y oriented?
+        private Node lb;        // the left/bottom subtree
+        private Node rt;        // the right/top subtree
+
+        public Node(Point2D p, boolean isXOriented) {
+            this.p = p;
+            this.isXOriented = isXOriented;
+        }
     }
+
+    Node root;
+
+    // construct an empty set of points
+    public KdTree() { }
 
     // is the set empty?
     public boolean isEmpty() {
-        return true;
+        return root == null;
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return size(root);
+    }
+
+    private int size(Node node) {
+        return node == null ? 0 : 1 + size(node.lb) + size(node.rt);
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
+
+        root = insert(root, p, false);
+    }
+
+    private Node insert(Node node, Point2D p, boolean parentIsXOriented) {
+        if(node == null) return new Node(p, !parentIsXOriented);
+
+        boolean insertIntoLeftBottom = node.isXOriented ? p.x() < node.p.x() : p.y() < node.p.y();
+        if (insertIntoLeftBottom) node.lb = insert(node.lb, p, node.isXOriented);
+        else                      node.rt = insert(node.rt, p, node.isXOriented);
+
+        return node;
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
 
-        return false;
+        return contains(root, p);
+    }
+
+    private boolean contains(Node node, Point2D p) {
+        if (node == null) return false;
+        if (node.p.equals(p)) return true;
+
+        boolean searchLeftBottom = node.isXOriented ? p.x() < node.p.x() : p.y() < node.p.y();
+        if (searchLeftBottom) return contains(node.lb, p);
+        else                  return contains(node.rt, p);
     }
 
     // draw all points to standard draw
