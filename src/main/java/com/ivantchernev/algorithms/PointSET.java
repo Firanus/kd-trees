4d3189ef16655359f03287a64639ce6d16e48b9a
Ivan Tchernev <itchernev@and.digital>
2018-10-07 15:59:37 +0100

    Initial (tested) implementation of PointSET

--- src/main/java/com/ivantchernev/algorithms/PointSET.java
+++ src/main/java/com/ivantchernev/algorithms/PointSET.java
@@ -3,6 +3,8 @@ package com.ivantchernev.algorithms;
 import java.util.TreeSet;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class PointSET {
 
@@ -12,11 +14,85 @@ public class PointSET {
     public PointSET() {
         treeSet = new TreeSet<>(Point2D.X_ORDER);
     }
-//    public           boolean isEmpty()                      // is the set empty?
-//    public               int size()                         // number of points in the set
-//    public              void insert(Point2D p)              // add the point to the set (if it is not already in the set)
-//    public           boolean contains(Point2D p)            // does the set contain point p?
-//    public              void draw()                         // draw all points to standard draw
-//    public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle (or on the boundary)
-//    public           Point2D nearest(Point2D p)             // a nearest neighbor in the set to point p; null if the set is empty
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return treeSet.isEmpty();
+    }
+
+    // number of points in the set
+    public int size() {
+        return treeSet.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        treeSet.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        return treeSet.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+
+        double minx= Double.MAX_VALUE;
+        double miny = Double.MAX_VALUE;
+        double maxx = -Double.MAX_VALUE;
+        double maxy = -Double.MAX_VALUE;
+
+        for (Point2D p : treeSet) {
+            if (p.x() < minx) minx = p.x();
+            if (p.y() < miny) miny = p.y();
+            if (p.x() > maxx) maxx = p.x();
+            if (p.y() > maxy) maxy = p.y();
+
+            p.draw();
+        }
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException();
+
+        Stack<Point2D> stack = new Stack<>();
+        for (Point2D p: treeSet) {
+            if (rect.contains(p)) stack.push(p);
+        }
+        return stack;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        Point2D nearestPoint = null;
+        double closestDistanceSquared = Double.MAX_VALUE;
+        for (Point2D storedPoint: treeSet) {
+            if (p.distanceSquaredTo(storedPoint) < closestDistanceSquared) {
+                nearestPoint = storedPoint;
+                closestDistanceSquared = p.distanceSquaredTo(storedPoint);
+            }
+        }
+
+        return nearestPoint;
+    }
+
+    public static void main(String[] args) {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(0.2,0.7));
+        set.insert(new Point2D(0.5,0.5));
+        set.insert(new Point2D(0.8,0.2));
+
+        set.draw();
+    }
 }
--- src/test/java/com/ivantchernev/algorithms/PointSETTest.java
+++ src/test/java/com/ivantchernev/algorithms/PointSETTest.java
@@ -1,7 +1,143 @@
 package com.ivantchernev.algorithms;
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointSETTest {
 
+    // isEmpty Tests
+
+    @Test
+    void isEmptyReturnsTrueWhenSetEmpty() {
+        assertTrue(new PointSET().isEmpty());
+    }
+
+    @Test
+    void isEmptyReturnsFalseWhenSetContainsItems() {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(0,0));
+
+        assertFalse(set.isEmpty());
+    }
+
+    // size tests
+
+    @Test
+    void sizeReturnsZeroForEmptySet() {
+        assertEquals(0, new PointSET().size());
+    }
+
+    @Test
+    void sizeReturnsCorrectNumberWhenSetContainsItems() {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(0,0));
+        set.insert(new Point2D(0.5,0.5));
+        set.insert(new Point2D(1,1));
+
+        assertEquals(3, set.size());
+    }
+
+    // insert tests
+
+    @Test
+    void insertThrowsIllegalArgumentExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new PointSET().insert(null));
+    }
+
+    // contains tests
+
+    @Test
+    void containsThrowsIllegalArgumentExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new PointSET().contains(null));
+    }
+
+
+    @Test
+    void containsReturnsFalseOnEmptySet() {
+        assertFalse(new PointSET().contains(new Point2D(0,0)));
+    }
+
+    @Test
+    void containsReturnsFalseWhenItemNotInSet() {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(0,0));
+        set.insert(new Point2D(0.5,0.5));
+        set.insert(new Point2D(1,1));
+
+        assertFalse(set.contains(new Point2D(2,2)));
+    }
+
+    @Test
+    void containsReturnsTrueWhenItemInSet() {
+        PointSET set = new PointSET();
+        Point2D p = new Point2D(1, 1);
+
+        set.insert(p);
+
+        assertTrue(set.contains(p));
+    }
+
+    @Test
+    void containsPerformsAValueTypeComparison() {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(1,1));
+
+        assertTrue(set.contains(new Point2D(1,1)));
+    }
+
+    // range tests
+
+    @Test
+    void rangeThrowsIllegalArgumentExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new PointSET().range(null));
+    }
+
+    @Test
+    void rangeContainsAllValuesInRect() {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(1,1));
+        set.insert(new Point2D(2,2));
+        set.insert(new Point2D(3,3));
+        set.insert(new Point2D(4,4));
+
+        Iterable<Point2D> range = set.range(new RectHV(2.5, 2.5, 3.5, 3.5));
+
+        range.forEach(point2D -> {
+            assertEquals(point2D, new Point2D(3,3));
+        });
+    }
+
+    // nearest tests
+
+    @Test
+    void nearestThrowsIllegalArgumentExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new PointSET().nearest(null));
+    }
+
+    @Test
+    void nearestReturnsNullIfSetEmpty() {
+        assertNull(new PointSET().nearest(new Point2D(1,1)));
+    }
+
+    @Test
+    void nearestReturnsNearestElement() {
+        PointSET set = new PointSET();
+
+        set.insert(new Point2D(1,1));
+        set.insert(new Point2D(2,2));
+        set.insert(new Point2D(3,3));
+        set.insert(new Point2D(4,4));
+
+        Point2D nearest = set.nearest(new Point2D(3.2,2.8));
+
+        assertEquals(nearest, new Point2D(3,3));
+    }
 }
